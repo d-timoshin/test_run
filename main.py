532c223248737bb53b964f0bf1ff79c9577a1803
Denis Timoshin <den.timoshin@kokoc.tech>
2024-06-28 22:52:50 +0300

    четверта фиксация

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
+# Остальной код...
 
 driver.get("https://dzen.ru") # Открываем страницу
 
