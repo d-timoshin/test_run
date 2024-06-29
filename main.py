30748c26308da23fdfc730d75a0584f90ac76b35
Denis Timoshin <den.timoshin@kokoc.tech>
2024-06-29 15:29:02 +0300

    пятая  фиксация

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pytest
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+@pytest.fixture
+def browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=chrome_options)
-# Остальной код...
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver  # Передаем управление тесту
+    driver.quit()  # Закрываем браузер после теста
 
-driver.get("https://dzen.ru") # Открываем страницу
+def test_open_page(browser):  # Принимаем фикстуру как аргумент
+    driver = browser  # Получаем экземпляр драйвера из фикстуры
 
-assert driver.title == "Дзен", "Страница не открылась"
+    driver.get("https://dzen.ru")  # Открываем страницу
 
-print("Страница открылась")
+    assert driver.title == "Дзен", "Страница не открылась"
+
+    print("Страница открылась")
